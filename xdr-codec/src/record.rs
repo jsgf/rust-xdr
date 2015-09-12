4d3413e3df4a82b3671a638f7c04cdd8bd228b8c
Jeremy Fitzhardinge <jeremy@goop.org>
2015-09-12 11:03:38 -0700

    Add documentation for xdr_codec::record.

--- xdr-codec/src/record.rs
+++ xdr-codec/src/record.rs
@@ -1,3 +1,17 @@
+//! XDR record marking
+//!
+//! This module implements wrappers for `Write` and `BufRead` which
+//! implement "Record Marking" from RFC1831, used for encoding XDR
+//! structures onto a bytestream such as TCP.
+//!
+//! The format is simple - each record is broken up into one or more
+//! record fragments. Each record fragment is prefixed with a 32-bit
+//! big-endian value. The low 31 bits is the fragment size, and the
+//! top bit is the "end of record" marker, indicating the last
+//! fragment of the record.
+//!
+//! There's no magic number or other way to determine whether a stream
+//! is using record marking; both ends must agree.
 use std::io::{self, Read, BufRead, Write};
 use std::cmp::min;
 use byteorder;
@@ -13,6 +27,12 @@ fn mapioerr(xdrerr: Error) -> io::Error {
     }
 }
 
+/// Read records from a bytestream.
+///
+/// Reads will read up to the end of the current fragment, and not
+/// beyond. The `BufRead` trait doesn't otherwise allow for record
+/// boundaries to be deliniated. Callers can use the `eor` method to
+/// determine record ends.
 pub struct XdrRecordReader<R: BufRead> {
     size: usize,                // record size
     consumed: usize,            // bytes consumed
@@ -22,6 +42,8 @@ pub struct XdrRecordReader<R: BufRead> {
 }
 
 impl<R: BufRead> XdrRecordReader<R> {
+    /// Wrapper a record reader around an existing implementation of
+    /// `BufRead`, such as `BufReader`.
     pub fn new(rd: R) -> XdrRecordReader<R> {
         XdrRecordReader {
             size: 0,
@@ -50,6 +72,7 @@ impl<R: BufRead> XdrRecordReader<R> {
 
     fn totremains(&self) -> usize { self.size - self.consumed }
 
+    /// Current fragment is the end of the record.
     pub fn eor(&self) -> bool {
         self.eor
     }
@@ -91,6 +114,9 @@ impl<R: BufRead> BufRead for XdrRecordReader<R> {
 
 const WRBUF: usize = 65536;
 
+/// Write records into a bytestream.
+///
+/// Flushes the current buffer as end of record when destroyed.
 pub struct XdrRecordWriter<W: Write> {
     buf: Vec<u8>,
     bufsz: usize,
@@ -98,7 +124,16 @@ pub struct XdrRecordWriter<W: Write> {
 }
 
 impl<W: Write> XdrRecordWriter<W> {
+    /// Create a new `XdrRecordWriter` wrapped around a `Write`
+    /// implementation, using a default buffer size (64k).
+    pub fn new(w: W) -> XdrRecordWriter<W> {
+        XdrRecordWriter::with_buffer(w, WRBUF)
+    }
+
+    /// Create an instance with a specific buffer size. Panics if the
+    /// size is zero.
     pub fn with_buffer(w: W, bufsz: usize) -> XdrRecordWriter<W> {
+        if bufsz == 0 { panic!("bufsz must be non-zero") }
         XdrRecordWriter {
             buf: Vec::with_capacity(bufsz),
             bufsz: bufsz,
@@ -106,10 +141,8 @@ impl<W: Write> XdrRecordWriter<W> {
         }
     }
 
-    pub fn new(w: W) -> XdrRecordWriter<W> {
-        XdrRecordWriter::with_buffer(w, WRBUF)
-    }
-    
+    /// Flush the current buffer. If `eor` is true, the end of record
+    /// marker is set.
     pub fn flush_eor(&mut self, eor: bool) -> io::Result<()> {
         if !eor && self.buf.len() == 0 { return Ok(()) }
 
@@ -216,6 +249,13 @@ mod test {
         let mut buf = [0; 100];
         assert_eq!(recread.read(&mut buf).unwrap(), 0);
         assert!(recread.eor());
+    }
+
+    #[test]
+    #[should_panic(expected="must be non-zero")]
+    fn zerosz() {
+        let buf = Vec::new();
+        let _ = XdrRecordWriter::with_buffer(buf, 0);
     }
 
     #[test]
