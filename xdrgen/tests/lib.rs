bb7ac6552ed0816cb866c01adc4b75bd2fe97c35
Jeremy Fitzhardinge <jeremy@goop.org>
2015-11-02 17:49:22 -0800

    Update end-to-end compile test
    - remove boilerplate from each test
    - use target build dir
    - use -Z no-trans to speed up test

--- xdrgen/tests/lib.rs
+++ xdrgen/tests/lib.rs
@@ -2,37 +2,57 @@ extern crate xdrgen;
 extern crate xdr_codec;
 
 use std::env;
-use std::fs;
-use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::fs::{create_dir_all, File};
+use std::path::PathBuf;
 use std::io::{Cursor, Write};
 use std::process::Command;
-use std::result;
 
 use xdrgen::{generate, compile};
 use xdr_codec::{Result, Error};
 
-fn verify_gen<P: AsRef<Path>>(xdr_spec: &str,
-                              prog_spec: &str,
-                              outspec: P,
-                              outprog: P,
-                              outexe: P)
-                              -> Result<()> {
-    let fspec = try!(File::create(outspec));
-    try!(generate("", Cursor::new(xdr_spec.as_bytes()), fspec));
-    let mut fprog = try!(File::create(&outprog));
-    try!(fprog.write_all(prog_spec.as_bytes()));
+fn build_test(name: &str, xdr_spec: &str) -> Result<()> {
+    let dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let _ = create_dir_all(&dir);
     
+    let mainfile = dir.with_file_name(format!("{}.rs", name));
+    let testfile = dir.with_file_name(format!("{}_xdr.rs", name));
+    let exefile = dir.with_file_name(format!("{}", name));
+
+    let template = format!(r#"
+extern crate xdr_codec;
+
+#[allow(dead_code, non_camel_case_types)]
+mod test {{
+    use xdr_codec;
+    include!("{}");
+}}
+
+fn main() {{}}
+"#,
+                           testfile.as_os_str().to_string_lossy());
+
+    {
+        let mut main = try!(File::create(&mainfile));
+        try!(main.write_all(template.as_bytes()));
+    }
+
+    {
+        let test = try!(File::create(&testfile));
+        try!(generate(name, Cursor::new(xdr_spec.as_bytes()), test));
+    }
+
     let compile = try!(Command::new("rustc")
-                       .arg("-L")
-                       .arg("target/debug/deps")
-                       .arg("-o")
-                       .arg(outexe.as_ref())
-                       .arg(outprog.as_ref())
+                       .arg("-L").arg(dir.with_file_name("../../deps"))
+                       .arg("-o").arg(exefile)
+                       .arg("-Z").arg("no-trans")
+                       .arg(mainfile)
                        .output());
+
     println!("stdout: {}\n, stderr: {}",
              String::from_utf8_lossy(&compile.stdout),
              String::from_utf8_lossy(&compile.stderr));
+
     if compile.status.success() {
         Ok(())        
     } else {
@@ -40,72 +60,24 @@ fn verify_gen<P: AsRef<Path>>(xdr_spec: &str,
     }
 }
 
-fn verbose_remove<P: AsRef<Path>>(path: P, msg: &str) {
-    if let result::Result::Err(e) = fs::remove_file(path) {
-        println!("{}: {:?}", msg, e);
-    }
-}
-
-// returns specpath, progpath, expath
-fn get_paths(name: &str) -> (PathBuf, PathBuf, PathBuf) {
-    let cdir = env::current_dir().unwrap();
-    let mut specpath = cdir.clone();
-    let mut progpath = cdir.clone();
-    let mut exepath = cdir.clone();
-
-    specpath.push(format!("tests/scratch/{}_xdr.rs", name));
-    progpath.push(format!("tests/scratch/{}.rs", name));
-    exepath.push(format!("tests/scratch/{}", name));
-    (specpath, progpath, exepath)
-}
-
 #[test]
 fn typedef_arrays() {
-    let prog = r#"
-extern crate xdr_codec;
-
-mod typedefs {
-    use xdr_codec;
-
-    #[allow(dead_code)]
-    include!("typedefs_xdr.rs");
-}
-
-fn main() {
-    println!("wheew");
-}
-"#;
+    let name = "typedef_arrays";
     let spec = r#"
 typedef opaque buf1<20>;
 typedef opaque buf2[10];
 typedef opaque buf3<>;
 "#;
 
-    let (outfile, outprog, outexe) = get_paths("typedefs");
-    let res = verify_gen(spec, prog, &outfile, &outprog, &outexe);
-    verbose_remove(&outfile, "error removing xdr spec");
-    verbose_remove(&outprog, "error removing prog");
-    verbose_remove(&outexe, "error removing exe");
-    assert!(res.is_ok());
+    if let Err(e) = build_test(name, spec) {
+        panic!("test {} failed: {}", name, e);
+    }
 }
 
 #[test]
 fn union_with_default() {
-    let prog = r#"
-extern crate xdr_codec;
-
-mod foo {
-    use xdr_codec;
-
-    #[allow(dead_code)]
-    include!("union_default_xdr.rs");
-}
-
-fn main() {
-    println!("wheew");
-}
-"#;
-    let xdr_spec = r#"
+    let name = "union_with_default";
+    let spec = r#"
 union foo switch (int bar) {
 case 1:
     int val;
@@ -113,31 +85,16 @@ default:
     void;
 };
 "#;
-    let (outfile, outprog, outexe) = get_paths("union_default");
-    let res = verify_gen(xdr_spec, prog, &outfile, &outprog, &outexe);
-    verbose_remove(&outfile, "error removing xdr spec");
-    verbose_remove(&outprog, "error removing prog");
-    verbose_remove(&outexe, "error removing exe");
-    assert!(res.is_ok())
+
+    if let Err(e) = build_test(name, spec) {
+        panic!("test {} failed: {}", name, e);
+    }
 }
 
 #[test]
 fn union_default_nonempty() {
-    let prog = r#"
-extern crate xdr_codec;
-
-mod foo {
-    use xdr_codec;
-
-    #[allow(dead_code)]
-    include!("union_default_nonempty_xdr.rs");
-}
-
-fn main() {
-    println!("wheew");
-}
-"#;
-    let xdr_spec = r#"
+    let name = "union_default_nonempty";
+    let spec = r#"
 union foo switch (int bar) {
 case 1:
     int val;
@@ -145,10 +102,8 @@ default:
     opaque buf<>;
 };
 "#;
-    let (outfile, outprog, outexe) = get_paths("union_default_nonempty");
-    let res = verify_gen(xdr_spec, prog, &outfile, &outprog, &outexe);
-    verbose_remove(&outfile, "error removing xdr spec");
-    verbose_remove(&outprog, "error removing prog");
-    verbose_remove(&outexe, "error removing exe");
-    assert!(res.is_ok())
+
+    if let Err(e) = build_test(name, spec) {
+        panic!("test {} failed: {}", name, e);
+    }
 }
